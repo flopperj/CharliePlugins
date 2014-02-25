e4823a7f5eed1b525e72a912247014adbe20c9c0
James Arama <safaribobth3intelchip@gmail.com>
2014-02-25 01:12:52 -0500

    Initial Commit - Added the Advisor stub for the advisor plugin

---
+++ src/charlie/advisor/Advisor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package charlie.advisor;
+
+import charlie.card.Card;
+import charlie.card.Hand;
+import charlie.plugin.IAdvisor;
+import charlie.util.Play;
+
+/**
+ *
+ * @author jamesarama
+ */
+public class Advisor implements IAdvisor {
+
+    @Override
+    public Play advise(Hand myHand, Card upCard) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
+
